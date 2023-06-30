d0fb4e7ec8c45b5e3079685e96c39b1f6467be0d
Rauf Guven <mrguven@gmail.com>
2023-06-30 13:23:22 +0200

    update

---
+++ src/components/UserRespo.jsx
@@ -0,0 +1,39 @@
+
+import { Link } from "react-router-dom";
+
+export default function UserRespo () {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return(
+<div>
+
+
+
+
+<div class="list-group">
+  <h1  className="list-group-item list-group-item-action active" aria-current="true">
+  User Resporitories
+  </h1>
+  <Link to className="list-group-item list-group-item-action">A second link item</Link>
+  <a href="#" className="list-group-item list-group-item-action">A third link item</a>
+  <a href="#" className="list-group-item list-group-item-action">A fourth link item</a>
+  <a className="list-group-item list-group-item-action disabled">A disabled link item</a>
+</div>
+
+
+</div>
+
+    )
+}
