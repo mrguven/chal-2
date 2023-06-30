841b9f34146982c575e2e49009206b86642e09bf
Rauf Guven <mrguven@gmail.com>
2023-06-30 17:00:59 +0200

    update

--- src/components/UserRespo.jsx
+++ src/components/UserRespo.jsx
@@ -29,8 +29,10 @@ const [respoList,setRespoList]=useState()
 
 
 
-
-
+<div class="input-group mb-3">
+  <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
+  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+</div>
 
 
 
