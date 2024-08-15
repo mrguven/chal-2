32469ad99cdbff5308e49766f8d91708dbeedf8a
Rauf GUVEN <59612808+mrguven@users.noreply.github.com>
2024-08-15 11:09:59 +0200

    Update UserProfile.jsx

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,24 +1,14 @@
-
 import { useEffect, useState } from "react";
 import axios from "axios";
-
-
 export default function UserProfile() {
-
     const [data,setData]=useState({
-      
-      
-       })
-
+    })
     const [fullName,setFullName]=useState('sdf');
     const [userName,setUserName]=useState('sdf');
     const [location,setLocation]=useState('sdf');
     const [userLink,setUserLink]=useState();
     const[userImage,setUserImage]=useState()
-
-
     useEffect( ()=> {
-
         axios.get(process.env.REACT_APP_GITHUB_API)
         .then((res)=> {console.log(res.data);const data=res.data ; setFullName(data.name);
             setLocation(data.location);
@@ -27,54 +17,29 @@ export default function UserProfile() {
             setUserImage(data.avatar_url);     }
         )
         .catch(err=> console.log(err))},[]
-    
     )
     
-
-
-
-
-
-
-    return(
-
-
-
-
+     return(
 <div>
-
-
-
 <img id="userImg" src={userImage} alt="" />
-
-
-
 <div id="userProfile">
 <ul className="list-group list-group-horizontal">
   <li className="list-group-item"><b> Full Name :</b></li>
   <li className="list-group-item">{fullName}</li>
-  
-</ul>
+  </ul>
 <ul className="list-group list-group-horizontal-sm">
   <li className="list-group-item"><b>User Name:</b></li>
   <li className="list-group-item">{userName}</li>
-  
-</ul>
+  </ul>
 <ul className="list-group list-group-horizontal">
   <li className="list-group-item"><b>Location:</b></li>
   <li className="list-group-item">{location} </li>
- 
-</ul>
+ </ul>
 <ul className="list-group list-group-horizontal">
   <li className="list-group-item"><b>User GITHUB Link:</b></li>
   <li className="list-group-item"><a href={userLink}>{userLink} </a></li>
- 
-</ul>
+ </ul>
 </div>
 </div>
-
-
-
-
     )
 }
